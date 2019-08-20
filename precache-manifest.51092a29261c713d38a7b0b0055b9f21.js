3eb3c8d76c30eb49ae8bb2a1c4076364e8f9ab6f
Sandro <sandrohc2010@hotmail.com>
2019-08-20 10:10:50 -0400

    406cd51

---
+++ precache-manifest.51092a29261c713d38a7b0b0055b9f21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/escolar/robots.txt"
+  },
+  {
+    "revision": "fd951f96af4268c080f7",
+    "url": "/escolar/js/chunk-vendors.40717e81.js"
+  },
+  {
+    "revision": "210e168376095c80a7e9",
+    "url": "/escolar/js/app.6fbfae68.js"
+  },
+  {
+    "revision": "4d00bf6bd3f8e25c8bdb",
+    "url": "/escolar/js/about.d5b70649.js"
+  },
+  {
+    "revision": "77b8d55d6b4a4afd3f9628050060de8c",
+    "url": "/escolar/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/escolar/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "fd951f96af4268c080f7",
+    "url": "/escolar/css/chunk-vendors.8957f339.css"
+  },
+  {
+    "revision": "4d00bf6bd3f8e25c8bdb",
+    "url": "/escolar/css/about.73ce9c4e.css"
+  }
+];
